86733c8043d82264161e2bf60473f536fe40edc1
BackinR2D2 <cristimlg334@gmail.com>
2020-08-15 16:31:45 +0300

    bug fix

--- server.js
+++ server.js
@@ -51,15 +51,20 @@ app.get('/search-player', async (req, res) => {
                 data = profileData.data.response.players
                 const recentGames = await axios.get(`http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${key}&steamid=${userId}&format=json`)
                 let games = recentGames.data.response.games;
-                res.render('searchPlayer', { data, games })
+                return res.render('searchPlayer', { data, games })
             } else if (isnum && userId.substring(0, 3) !== '765') {
                 let data;
                 let bit64id = '765' + (Number(userId) + 61197960265728)
                 const stats = await axios.get(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${key}&steamids=${bit64id}`)
                 data = stats.data.response.players
+                
                 const recentGames = await axios.get(`http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${key}&steamid=${bit64id}&format=json`)
                 let games = recentGames.data.response.games;
-                res.render('searchPlayer', { data, games })
+                if(data.length === 0 && typeof games === 'undefined') {
+                    return res.render('searchPlayer', {data: 'Not found!', games: ''})
+                } else {
+                    return res.render('searchPlayer', { data, games })    
+                }
             } else if (userId.includes('id/')) {
                 let ID = userId.split('id/')[1]
                 const profileID = await axios.get(`http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${key}&vanityurl=${ID}`)
@@ -68,17 +73,17 @@ app.get('/search-player', async (req, res) => {
                 let data = steamInfo.data.response.players
                 const recentGames = await axios.get(`http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${key}&steamid=${steamid}&format=json`)
                 let games = recentGames.data.response.games;
-                res.render('searchPlayer', { data, games })
+                return res.render('searchPlayer', { data, games })
             } else {
-                res.render('searchPlayer', { data: 'Not found!', games: '' })
+                return res.render('searchPlayer', { data: 'Not found!', games: '' })
             }
         } catch (error) {
             if (error.response.status === 500) {
-                res.render('searchPlayer', { data: 'Not Found!', games: '' })
+                return res.render('searchPlayer', { data: 'Not Found!', games: '' })
             }
         }
     } else if (userId === undefined) {
-        res.render('searchPlayer', { data: typeof userId, games: typeof userId })
+        return res.render('searchPlayer', { data: typeof userId, games: typeof userId })
     }
 })
 
@@ -89,3 +94,64 @@ app.use((req, res) => {
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 })
+
+/*
+
+'<% if (heroUnique.includes(data.hero_id)) { %>'
+        '<% let index = heroUnique.indexOf(data.hero_id) %>'
+        heroName.textContent = '<%= heroes[index].localized_name %>'
+        '<% } %>'
+ */
+
+ /*
+ 
+ 
+        const headers = document.querySelectorAll('h1')
+        let heroNames = []
+        headers.forEach(header => {
+            heroNames.push(header.innerText)
+        })
+        console.log(heroNames);
+        let heroIMGS = []
+        let hnames = []
+        let show
+        let heroimages = []
+        fetch('heroes.json')
+            .then((resp) => resp.json())
+            .then((heroes) => {
+                heroes.heroes.map((heroImg) => {
+                    heroIMGS.push({ id: heroImg.id, heroname: heroImg.localized_name, heroimg: heroImg.url_small_portrait })
+                })
+
+                heroIMGS.map(hero => {
+                    // console.log(hero);
+                    // if (heroNames.indexOf(hero.heroname) !== -1) {
+                    //     // let hname = hero.heroimg.split('/heroes/')[1]
+                    //     // let name = hname.split('_sb')[0]
+                    //     console.log(hero.heroname);
+                    //     // console.log(hero.heroimg.includes(hero.heroname.toLowerCase()));
+                    // }
+                    // let hnames = []
+                    if (heroNames.includes(hero.heroname)) {
+                        // console.log(hero.heroname);
+
+                        hnames.push(hero.heroname)
+                        show = hnames.sort(function (a, b) {
+                            return heroNames.indexOf(a) - heroNames.indexOf(b);
+                        });
+                        show.forEach(name => {
+                            if (name === hero.heroname) {
+                                heroimages.push(hero.heroimg)
+                            }
+                        })
+                        heroimages.forEach(heroimage => {
+                            
+                        })
+                    }
+                })
+
+            })
+
+
+ 
+ */
